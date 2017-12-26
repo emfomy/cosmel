dff410dae357690490215a4fafce677028bbd318
Mu Yang <emfomy@gmail.com>
2017-12-26 18:10:32 +0800

    Updates preprocessing.

--- ipython/study_sinica_muyang/script/repo.sh
+++ ipython/study_sinica_muyang/script/repo.sh
@@ -10,12 +10,10 @@ mkdir -p $TMP
 
 echo '␣	SP	2147483647' > $DIR/sp.lex
 
-sed 's/ /␣/g' $DIR/list_n_brand.txt > $DIR/list_n_brand.lex
+sed 's/	N_Product//g' $DIR/products.lex > $TMP/products.ns.lex
+CKIPWSTester script/brands.ini $TMP/products.ns.lex $TMP/products.ns.tag
+sed 's/　␣(SP)//g' $TMP/products.ns.tag > $DIR/products.tag
 
-sed 's/ /␣/g;s/	N_Product//g' $DIR/list_n_product.txt > $TMP/list_n_product.ns.txt
-CKIPWSTester script/brand.ini $TMP/list_n_product.ns.txt $TMP/list_n_product.ns.tag
-sed 's/　␣(SP)//g' $TMP/list_n_product.ns.tag > $DIR/list_n_product.tag
-
-sed 's/ /␣/g;s/	N_CProduct//g' $DIR/list_n_cproduct.txt > $TMP/list_n_cproduct.ns.txt
-CKIPWSTester script/brand.ini $TMP/list_n_cproduct.ns.txt $TMP/list_n_cproduct.ns.tag
-sed 's/　␣(SP)//g' $TMP/list_n_cproduct.ns.tag > $DIR/list_n_cproduct.tag
+sed 's/	N_CProduct//g' $DIR/cproducts.lex > $TMP/cproducts.ns.lex
+CKIPWSTester script/brands.ini $TMP/cproducts.ns.lex $TMP/cproducts.ns.tag
+sed 's/　␣(SP)//g' $TMP/cproducts.ns.tag > $DIR/cproducts.tag
