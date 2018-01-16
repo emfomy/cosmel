7b5f71ab8259a66faddce0f484b0c3a3baaf9890
Mu Yang <emfomy@gmail.com>
2018-01-16 16:20:05 +0800

    Add mention proprecessing.

--- ipython/study_sinica_muyang/test.py
+++ ipython/study_sinica_muyang/test.py
@@ -9,10 +9,9 @@
 from styleme import *
 
 if __name__ == '__main__':
-	# repo = Repo('data/repo')
-	# corpus = Corpus('data/article/prune_article_ws/part-00000/winnie227520_266565404.txt.tag', repo)
-	# for _, a in corpus.id2article.items():
-	# 	print(a.a_id)
-	print(len(WsWords('\n')))
+	repo = Repo('data/repo')
+	corpus = Corpus('data/article/prune_article_ws/part-00000/winnie227520_266565404.txt.tag', repo)
+	for _, a in corpus.id2article.items():
+		print(a.a_id)
 
 	pass
