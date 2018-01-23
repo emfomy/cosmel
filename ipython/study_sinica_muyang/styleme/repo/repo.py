ebb930a6e8c5e4dead3f69b3b74d689077628443
Mu Yang <emfomy@gmail.com>
2018-01-23 14:28:49 +0800

    Fix typo.

--- ipython/study_sinica_muyang/styleme/repo/repo.py
+++ ipython/study_sinica_muyang/styleme/repo/repo.py
@@ -19,13 +19,13 @@ class Repo:
 	"""
 
 	def __init__(self, repo_path):
-		self.__brand_set                 = BrandSet(repo_path)
-		self.__name_to_brand             = Name2Brand(self.__brand_set)
+		self.__brand_set                  = BrandSet(repo_path)
+		self.__name_to_brand              = Name2Brand(self.__brand_set)
 
-		self.__product_set               = ProductSet(repo_path, self.__name_to_brand)
-		self.__id_to_product             = Id2Product(self.__product_set)
-		self.__brandname_to_product      = BrandName2Product(self.__product_set)
-		self.__brandhead_to_product_list = BrandHead2ProductList(self.__product_set)
+		self.__product_set                = ProductSet(repo_path, self.__name_to_brand)
+		self.__id_to_product              = Id2Product(self.__product_set)
+		self.__brand_name_to_product      = BrandName2Product(self.__product_set)
+		self.__brand_head_to_product_list = BrandHead2ProductList(self.__product_set)
 
 	@property
 	def brand_set(self):
@@ -48,11 +48,11 @@ class Repo:
 		return self.__id_to_product
 
 	@property
-	def brandname_to_product(self):
+	def brand_name_to_product(self):
 		""":class:`.BrandName2Product`: the dictionary maps brand and name to product."""
-		return self.__brandname_to_product
+		return self.__brand_name_to_product
 
 	@property
-	def brandhead_to_product_list(self):
+	def brand_head_to_product_list(self):
 		""":class:`.BrandHead2ProductList`: the dictionary maps brand and head to product list."""
-		return self.__brandhead_to_product_list
+		return self.__brand_head_to_product_list
