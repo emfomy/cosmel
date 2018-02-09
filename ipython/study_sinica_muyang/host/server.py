c12e074c7b7cea58e50161905d63344c93c0e5a3
Mu Yang <emfomy@gmail.com>
2018-02-09 14:59:13 +0800

    Add annotation tool.

---
+++ ipython/study_sinica_muyang/host/server.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+"""
+.. codeauthor::
+   Mu Yang <emfomy@gmail.com>
+"""
+
+from flask import Flask
+from flask import redirect
+from flask import render_template
+from flask import request
+
+import json
+import os
+import time
+
+app = Flask(__name__)
+
+@app.route('/')
+def index_route():
+	global files
+	aid = request.args.get('aid')
+	return render_template('index.html', aid=aid, files=files)
+
+@app.route('/prev')
+def prev_route():
+	global files
+	try:
+		idx = files.index(request.args.get('aid')) - 1
+	except ValueError:
+		idx = 0
+	aid = files[idx] if idx >= 0 else None
+	return redirect(f'/?aid={aid}', code=302)
+
+@app.route('/next')
+def next_route():
+	global files
+	try:
+		idx = files.index(request.args.get('aid')) + 1
+	except ValueError:
+		idx = 0
+	aid = files[idx] if idx < len(files) else None
+	return redirect(f'/?aid={aid}', code=302)
+
+@app.route('/article/<path:path>')
+def article_route(path):
+	return render_template(f'article/{path}.xml.html')
+
+@app.route('/json/<path:path>')
+def json_route(path):
+	with open(f'json/{path}.json') as fin:
+		data = fin.read().replace('\n', '<br/>')
+	return str(data)
+
+@app.route('/save', methods=['GET', 'POST'])
+def save_route():
+	data = request.data;
+	aid = request.args.get('aid')
+	json_data = json.loads(data)
+	d = time.time()
+	json_data['time'] = d
+	json_data['date'] = time.strftime('%a %b %d %Y %H:%M:%S GMT%z (%Z)', time.localtime(d))
+	print(aid, json_data)
+	file = f'json/{aid}.json'
+	os.makedirs(os.path.dirname(file), exist_ok=True)
+	with open(file, 'a') as fout:
+		fout.write(json.dumps(json_data)+'\n')
+	return 'success'
+
+if __name__ == '__main__':
+	global files
+	root  = 'article'
+	part  = 'part-00000'
+	ext   = '.xml.html'
+	files = sorted([f'{part}/{file}'.replace(ext, '') for file in os.listdir(f'templates/{root}/{part}') if file.endswith(ext)])
+	app.run(host='140.109.19.229', debug=True)
